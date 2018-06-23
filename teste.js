8d68c283db803f5a5d65ce976cdbd64d6695cf25
Thales <thales.ribeiro@engenharia.ufjf.br>
2018-06-23 11:03:32 -0300

    teste

--- teste.js
+++ teste.js
@@ -4,8 +4,8 @@ salt = crypto.randomFillSync(buf).toString('hex');
 console.log(salt);
 
 key1 = crypto.pbkdf2Sync('secret1', salt, 100000, 64, 'sha512');
-key2 = crypto.pbkdf2Sync('secret1', salt, 100000, 64, 'sha512');
+key2 = crypto.pbkdf2Sync('secret1', salt, 100001, 64, 'sha512');
 
 console.log(key1.toString('hex'));
 console.log(key2.toString('hex'));
-console.log(key1.toString('hex') == key1.toString('hex'));
+console.log(key1.toString('hex') == key2.toString('hex'));
