4bb5e3ac8808075fd198b4ec0399e67b0be9a32a
Thales <thales.ribeiro@engenharia.ufjf.br>
2018-07-07 11:26:30 -0300

    atividade

---
+++ nodejs-mqtt.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var mqtt = require('mqtt')
+var fs = require('fs')
+var path = require('path')
+var PORT = 1883  // troque p/ 8883 quando usar SSL/TLS
+var HOST = 'localhost'
+
+var options = {
+  port: PORT,
+  host: HOST,
+  username: 'stihler',
+  password: 'teste123',
+ // rejectUnauthorized: false,          // se o certificado fosse legítimo, poderia mudar pra true
+ // ca: [ fs.readFileSync('ca.crt') ],  // seria o certificado da autoridade, mas o node esta ignorando
+ // protocol: 'ssl'
+}
+
+var client = mqtt.connect(options)
+
+//client.subscribe('teste')
+client.publish('teste', 'Current time is: ' + new Date())
+client.on('message', function (topic, message) {
+  console.log(message.toString())
+})
+
+client.on('connect', function () {
+  console.log('Connected')
+})
